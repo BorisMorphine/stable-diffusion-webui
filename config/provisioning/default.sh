cb8aed02061c2da4b429cf97aa78cf73969ad121
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-19 22:09:21 -0500

    default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -2,7 +2,7 @@
 # This file will be sourced in init.sh
 # Namespace functions with provisioning_
 
-# https://github.com/Roldondo/stable-diffusion-webui/edit/main/config/provisioning/default.sh
+# https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/config/provisioning/default.sh
 
 ### Edit the following arrays to suit your workflow - values must be quoted and separated by newlines or spaces.
 
