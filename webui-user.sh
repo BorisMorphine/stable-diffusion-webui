b1c7e61dffe8fa7d61f586fea6db9c4cfb6f492c
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 17:45:28 -0500

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -4,31 +4,32 @@
 #########################################################
 
 # Install directory without trailing slash
-#install_dir="/home/$(whoami)"
+install_dir="/opt/stable-diffusion-webui-forge"
 
 # Name of the subdirectory
-#clone_dir="stable-diffusion-webui"
+clone_dir="/opt/stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="autostart"
 
 # python3 executable
-#python_cmd="python3"
+python_cmd="python3 pip install -r requirements.txt, pip install -r requirements-test.txt,
+pip install -r requirements_NPU.txt, pip install -r requirements_versions.txt"
 
 # git executable
 #export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+venv_dir="webui"
 
 # script to launch to start the app
-#export LAUNCH_SCRIPT="launch.py"
+export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
 # Requirements file to use for stable-diffusion-webui
-#export REQS_FILE="requirements_versions.txt"
+export REQS_FILE="requirements.txt"
 
 # Fixed git repos
 #export K_DIFFUSION_PACKAGE=""
@@ -40,7 +41,7 @@
 #export BLIP_COMMIT_HASH=""
 
 # Uncomment to enable accelerated launch
-#export ACCELERATE="True"
+export ACCELERATE="True"
 
 # Uncomment to disable TCMalloc
 #export NO_TCMALLOC="True"
