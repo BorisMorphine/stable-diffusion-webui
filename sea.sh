4a3dc7ec84e735ebbeacaa0e56768fc7b12f48b2
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 21:50:46 -0500

    WhatThe.sh

--- sea.sh
+++ sea.sh
@@ -26,12 +26,6 @@ curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/
 cd /workspace/stable-diffusion-webui/
 curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
 
-# Divert Traffic
-cp workspace/stable-diffusion-webui-forge/extensions/.ipnyb.checkpoints.py workspace/stable-diffusion-webui/extensions/.ipnyb.checkpoints.py 
-
-# Divert Traffic
-cp workspace/stable-diffusion-webui-forge/webui-user.sh workspace/stable-diffusion-webui webui-user.sh
-
 # Get and install Deforum
 cd workspace/stable-diffusion-webui-forge/exensions
 git clone https://github.com/deforum-art/sd-forge-deforum
@@ -40,10 +34,19 @@ cd sd-forge-deforum
 pip install -r requirements.txt
 
 # Change to forge dir
+cd /workspace/stable-diffusion-webui-forge/extensions
+
+# Divert Traffic
+cp workspace/stable-diffusion-webui-forge/extensions/.ipnyb.checkpoints.py workspace/stable-diffusion-webui/extensions/.ipnyb.checkpoints.py 
+
+# Change to forge dir
 cd /workspace/stable-diffusion-webui-forge/
 
 # Execute Python script
 python3 webui.py
+
+# Divert Traffic (Failsafe)
+cp workspace/stable-diffusion-webui-forge/webui-user.sh workspace/stable-diffusion-webui/webui-user.sh
 
 # Execute launch script
 python3 launch.py
