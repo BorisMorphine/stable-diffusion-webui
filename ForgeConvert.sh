35d9ccdbf9b069fa52450258a182a5dca9e339d9
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 22:04:29 -0500

    Update ForgeMorphine.sh

--- ForgeConvert.sh
+++ ForgeConvert.sh
@@ -17,13 +17,7 @@ pip install -r requirements_versions.txt
 pip install -r requirements_npu.txt
 pip install -r requirements-test.txt
 
-if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
-
 # Download the required script into the current directory
-curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
-
-# Change to forge dir
-cd /workspace/stable-diffusion-webui/
 curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
 
 # Get and install Deforum
@@ -33,20 +27,18 @@ git clone https://github.com/deforum-art/sd-forge-deforum
 cd sd-forge-deforum
 pip install -r requirements.txt
 
-# Change to forge dir
-cd /workspace/stable-diffusion-webui-forge/extensions
-
 # Divert Traffic
-cp workspace/stable-diffusion-webui-forge/extensions/.ipnyb.checkpoints.py workspace/stable-diffusion-webui/extensions/.ipnyb.checkpoints.py 
+cd /workspace/stable-diffusion-webui/
+curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
 
-# Change to forge dir
+# Change back to forge dir
 cd /workspace/stable-diffusion-webui-forge/
 
-# Execute Python script
+# Dry run
 python3 webui.py
 
-# Divert Traffic (Failsafe)
-cp workspace/stable-diffusion-webui-forge/webui-user.sh workspace/stable-diffusion-webui/webui-user.sh
+# Cleanup
+if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
 # Execute launch script
 python3 launch.py
