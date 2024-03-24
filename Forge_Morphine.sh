f1b76ed818756aa6497ac7f3221ed39a7c9ab943
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-24 00:21:55 -0500

    Update Forge_Morphine.sh

--- Forge_Morphine.sh
+++ Forge_Morphine.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 ########################_UNCOMMIT_AND_ADD_TO_'ON_STARTUP'_COMMANDS_####################################################
-#echo $PATH
-#curl sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/Forge_Morphine.sh sudo | bash
+#echo $PATH; curl sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/Forge_Morphine.sh sudo | bash
 #if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
-#-e WEBUI_FLAGS="--data-dir="/opt" --autolaunch" 
-#supervisorctrl start webui
+#supervisorctrl restart webui
 ########################_UNCOMMIT_AND_ADD_TO_'ON_STARTUP'_COMMANDS_####################################################
 
 # Go to root
