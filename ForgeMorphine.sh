efe4ee748541b84e38171b330b6b3b346e3b241a
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 23:20:15 -0500

    Update ForgeMorphine.sh

--- ForgeMorphine.sh
+++ ForgeMorphine.sh
@@ -3,13 +3,13 @@
 echo $PATH
 
 # Go to data dir
-cd workspace
+cd /workspace
 
 # Clone the repository into a temporary directory 
 git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git
 
 # Change to forge dir
-cd /workspace/stable-diffusion-webui-forge/
+cd /workspace/stable-diffusion-webui-forge
 
 # Download the required web-ui script into the current directory
 curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
@@ -24,21 +24,33 @@ pip install -r requirements-test.txt
 if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
 # Get and install Deforum
-cd workspace/stable-diffusion-webui-forge/exensions
+cd /workspace/stable-diffusion-webui-forge/extensions
 git clone https://github.com/deforum-art/sd-forge-deforum
 
-cd sd-forge-deforum
+cd /sd-forge-deforum
 pip install -r requirements.txt
 
 # Cleanup
 if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
 # Divert Traffic
-cd /workspace/stable-diffusion-webui/
+cd /workspace/stable-diffusion-webui
 curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
 
+# throw in some shiny new upscalers..
+cd /workspace/storage/stable_diffusion/models/esrgan
+curl -o 4xUltraSharp.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/vRYVhaDA
+curl -o 4xUltraMix_Balanced.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
+curl -o 4xUltraMix_Restore.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
+curl -o 4xUltraMix_Smooth.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/PIRDEYgT
+curl -o 4x-FSDedither.pth https://drive.google.com/uc?export=download&confirm=1&id=1H4KQyhcknOoExjvDdsoxAgTBMO7JuJ3w
+curl -o Bendel_Halftone.pth https://drive.google.com/uc?export=download&confirm=1&id=1vR_tvWNi8jXhXdmgW5xvWsQp0pXN3ge-
+curl -o 8xESRGAN.pth https://icedrive.net/1/43GNBihZyi
+curl -o 16xESRGAN.pth https://objectstorage.us-phoenix-1.oraclecloud.com/n/ax6ygfvpvzka/b/open-modeldb-files/o/16x-ESRGAN.pth
+curl -o 16xPNSR.pth https://openmodeldb.info/models/16x-PSNR#:~:text=Download%20(64.1,by%20Google%20Drive
+
 # Change back to forge dir
-cd /workspace/stable-diffusion-webui-forge/
+cd /workspace/stable-diffusion-webui-forge
 
 # Dry run
 python3 webui.py
