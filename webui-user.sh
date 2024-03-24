7a65c771755a25752845582c0df7b3b92f80b8bc
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 23:21:40 -0500

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-install_dir="workspace"
+install_dir="/workspace"
 
-clone_dir="stable-diffusion-webui-forge"
+clone_dir="/stable-diffusion-webui-forge"
 
 venv_dir="/opts/micromamba/envs/webui/"
 
 export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
-export REQS_FILE="requirements.txt"
+export REQS_FILE="/stable-diffusion-webui-forge/requirements.txt"
 
 # Fixed git repos
 export K_DIFFUSION_PACKAGE="pip install k-diffusion"
@@ -18,10 +18,11 @@ export SD_KIT="pip install sdkit"
 
 export COMMANDLINE_ARGS="
 --autostart
---ckpt-dir workspace/stable-diffusion-webui-forge/models/ckpt
---hypernetwork-dir workspace/stable-diffusion-webui-forge/models/hypernetworks
---embeddings-dir workspace/stable-diffusion-webui-forge/embeddings
---lora-dir workspace/stable-diffusion-webui-forge/models/lora
+--ckpt-dir /workspace/stable-diffusion-webui-forge/models/ckpt
+--hypernetwork-dir /workspace/stable-diffusion-webui-forge/models/hypernetworks
+--embeddings-dir w/orkspace/stable-diffusion-webui-forge/embeddings
+--lora-dir /workspace/stable-diffusion-webui-forge/models/lora
+--esrgan-dir /workspace/storage/stable_diffusion/models/esrgan
 "
 
 #export ACCELERATE="True"
