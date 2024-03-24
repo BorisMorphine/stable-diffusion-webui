6f04652d6c2d9d98fd3ce39232378785960dc821
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 23:07:09 -0500

    Update ForgeMorphine.sh

--- ForgeMorphine.sh
+++ ForgeMorphine.sh
@@ -11,14 +11,17 @@ git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git
 # Change to forge dir
 cd /workspace/stable-diffusion-webui-forge/
 
+# Download the required web-ui script into the current directory
+curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
+
 # Install requirement
 pip install -r requirements.txt
 pip install -r requirements_versions.txt
 pip install -r requirements_npu.txt
 pip install -r requirements-test.txt
 
-# Download the required script into the current directory
-curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
+# Cleanup
+if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
 # Get and install Deforum
 cd workspace/stable-diffusion-webui-forge/exensions
@@ -26,6 +29,9 @@ git clone https://github.com/deforum-art/sd-forge-deforum
 
 cd sd-forge-deforum
 pip install -r requirements.txt
+
+# Cleanup
+if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
 # Divert Traffic
 cd /workspace/stable-diffusion-webui/
@@ -36,9 +42,6 @@ cd /workspace/stable-diffusion-webui-forge/
 
 # Dry run
 python3 webui.py
-
-# Cleanup
-if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
 # Execute launch script
 python3 launch.py
