3d483c2f0f80bc778cec9a100cf425277a36b026
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-23 23:06:32 -0500

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-install_dir="workspace/stable-diffusion-webui-forge"
+install_dir="workspace"
 
-clone_dir="workspace/stable-diffusion-webui"
+clone_dir="stable-diffusion-webui-forge"
 
-venv_dir="/opt/micromamba/envs/webui/"
+venv_dir="/opts/micromamba/envs/webui/"
 
 export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
