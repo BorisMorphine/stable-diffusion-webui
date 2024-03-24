73bdfcbaf7eec8899f30ebc70e4120577d641943
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-24 01:00:58 -0500

    Update Forge_Morphine.sh

--- Forge_Morphine.sh
+++ Forge_Morphine.sh
@@ -8,34 +8,32 @@
 # Go to root
 cd /root
 
--e data_dir="workspace"
-
--e install_dir="/opt"
-
--e clone_dir="/opt/stable-diffusion-webui-forge"
-
--e venv_dir="/opt/micromamba/envs/webui"
-
-# Go to install dir
-cd/opt
-
 # Clone the repository into a temporary directory 
-git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git
+git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git 
 
-# Change to the new forge dir...
-cd /opt/stable-diffusion-webui-forge
+# cd into the new repo...
+cd /root/stable-diffusion-webui-forge
 
 # now, download the required web-ui script into the current directory...
 curl -sS https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/webui-user.sh -o webui-user.sh
 
-# and install requirements...
+# and install requirements.
 pip install -r requirements.txt
 pip install -r requirements_versions.txt
 pip install -r requirements_npu.txt
 pip install -r requirements-test.txt
 
+# Most important guy in the room.
+-e webui_dir=/root/stable-diffusion-webui-forge
+-e models_dir=${webui_dir}/models
+-e sd_models_dir=${models_dir}/Stable-diffusion
+-e extensions_dir=${webui_dir}/extensions
+-e cn_models_dir=${extensions_dir}/sd-webui-controlnet/models
+-e vae_models_dir=${models_dir}/VAE
+-e upscale_models_dir=${models_dir}/ESRGAN
+
 # throw in some shiny new upscalers...
-cd /opt/storage/stable_diffusion/models/esrgan
+cd ${ESRGAN}
 curl -o 4xUltraSharp.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/vRYVhaDA
 curl -o 4xUltraMix_Balanced.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
 curl -o 4xUltraMix_Restore.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
@@ -47,20 +45,23 @@ curl -o 16xESRGAN.pth https://objectstorage.us-phoenix-1.oraclecloud.com/n/ax6yg
 curl -o 16xPNSR.pth https://openmodeldb.info/models/16x-PSNR#:~:text=Download%20(64.1,by%20Google%20Drive
 
 # get and install Deforum...
-cd /opt/stable-diffusion-webui-forge/extensions
+cd ${extensions_dir}
 git clone https://github.com/deforum-art/sd-forge-deforum.git
 
-cd /opt/stable-diffusion-webui-forge/extensions/sd-forge-deforum
+cd ${extensions_dir}/sd-forge-deforum
 pip install -r requirements.txt
 
-# change back to forge dir...
-cd /opt/stable-diffusion-webui-forge
-
-# and cleanup.
+# change back to forge dir and cleanup...
+cd ${webui_dir}
 echo $PATH
 if [ -z "${PATH-}" ]; then export PATH=/workspace/home/user/.local/bin; fi
 
-# Dry run
+# export all
+mkdir /opt/stable-diffusion-webui-forge
+cp /root/stable-diffusion-webui-forge /opt/stable-diffusion-webui-forge
+cd ${webui_dir}
+
+# dry-run
 python3 webui.py
 
 # Execute launch script
