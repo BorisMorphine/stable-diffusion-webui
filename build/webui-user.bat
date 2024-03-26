798d18435fa87eae6ec48ff7cdbc6b219b8cb58f
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-26 11:38:30 -0500

    Update webui-user.bat

--- build/webui-user.bat
+++ build/webui-user.bat
@@ -1,16 +1,2 @@
 @echo off
 
-set PYTHON="3.10.14"
-set GIT=
-set VENV_DIR=/workspace/stable-diffusion-webui/
-set COMMANDLINE_ARGS="--reinstall-torch"
-
-set A1111_HOME=/opt/stable-diffusion-webui/
-set VENV_DIR=/workspace/stable-diffusion-webui/
-set COMMANDLINE_ARGS="--xformers --enable-insecure-extension-access --api --deforum-api"
-set ckpt-dir /opt/storage/stable_diffusion/models/ckpt
-set hypernetwork-dir /opt/storage/stable_diffusion/models/hypernetworks
-set embeddings-dir /opt/storage/stable_diffusion/models//embeddings
-set lora-dir /opt/storage/stable_diffusion/models/models/Lora
-
-call webui.bat
