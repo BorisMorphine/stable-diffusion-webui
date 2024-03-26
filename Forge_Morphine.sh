00593126c146d94ff9283102ddd84449ca41d5c4
Roldondo <123861127+Roldondo@users.noreply.github.com>
2024-03-26 14:12:26 -0500

    Update Forge_Morphine.sh

--- Forge_Morphine.sh
+++ Forge_Morphine.sh
@@ -6,8 +6,7 @@
 cd /
 pip install opencv-python imageio imageio-ffmpeg onnxruntime pymatting pooch ezsynth GitPython Pillow accelerate blendmodes clean-fid einops facexlib fastapi>=0.90.1 gradio==3.41.2 inflection jsonmerge kornia lark numpy omegaconf open-clip-torch piexif psutil pytorch_lightning requests resize-right safetensors scikit-image>=0.19 tomesd torch torchdiffeq torchsde transformers==4.30.2 pytest-base-url~=2.0 pytest-cov~=4.0 pytest~=7.3 cloudpickle decorator synr==0.5.0 tornado
 
-mkdir workspace
-cd /workspace
+cd /opt
 git remote add forge https://github.com/lllyasviel/stable-diffusion-webui-forge
 git branch lllyasviel/main
 git checkout lllyasviel/main
@@ -23,18 +22,18 @@ git fetch forge
 git branch -u forge-master
 git pull
 
-cd /workspace/forge/extensions
+cd /opt/forge/extensions
 git clone Deforum https://github.com/deforum-art/sd-forge-deforum.git
 cd Deforum
 pip install -r requirements.txt
 
-cd /workspace/forge/extensions
+cd /opt/forge/extensions
 git clone ControlNet https://github.com/lllyasviel/ControlNet-v1-1-nightly.git
 cd/ControlNet
 git lfs install
 git clone https://huggingface.co/lllyasviel/sd_control_collection
 
-cd /workspace/forge
+cd /opt/forge
 wget -O https://raw.githubusercontent.com/Roldondo/stable-diffusion-webui/main/heavy-user.sh sudo | bash
 python3 launch.py
 
