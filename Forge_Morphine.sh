df079d369d24203c15116e3ac4b0218633b4a5d0
BorisMorphine <123861127+BorisMorphine@users.noreply.github.com>
2024-03-30 10:58:18 -0500

    Update Forge_Morphine.sh

--- Forge_Morphine.sh
+++ Forge_Morphine.sh
@@ -3,7 +3,14 @@
 ##############################################################################################
 #!/bin/bash
 
-apt install nano -y
+sudo git remote add forge https://github.com/lllyasviel/stable-diffusion-webui-forge
+git branch lllyasviel/main
+git checkout lllyasviel/main
+git fetch forge
+git branch -u forge/main
+git pull
+
+sudo apt install nano -y
 nano /workspace/stable-diffusion-webui/ui-config.json
 
 {
@@ -14,13 +21,6 @@ nano /workspace/stable-diffusion-webui/ui-config.json
 cd /
 pip install opencv-python imageio imageio-ffmpeg onnxruntime pymatting pooch ezsynth GitPython Pillow accelerate blendmodes clean-fid einops facexlib fastapi>=0.90.1 gradio==3.41.2 inflection jsonmerge kornia lark numpy omegaconf open-clip-torch piexif psutil pytorch_lightning requests resize-right safetensors scikit-image>=0.19 tomesd torch torchdiffeq torchsde transformers==4.30.2 pytest-base-url~=2.0 pytest-cov~=4.0 pytest~=7.3 cloudpickle decorator synr==0.5.0 tornado
 
-sudo git remote add forge https://github.com/lllyasviel/stable-diffusion-webui-forge
-git branch lllyasviel/main
-git checkout lllyasviel/main
-git fetch forge
-git branch -u forge/main
-git pull
-
 cd /extensions
 git remote add animateddiff https://github.com/continue-revolution/sd-forge-animatediff
 git branch forge-master
@@ -30,24 +30,30 @@ git branch -u forge-master
 git pull
 
 # Setting download directory paths: 
-set safetensors="/opt/stable-diffusion-webui/models/Stable-diffusion"
-set codeformer_models="/opt/stable-diffusion-webui/models/Codeformer"
-set controlnet_modles="/opt/stable-diffusion-webui/models/ControlNet"
-set deepbooru_models="/opt/stable-diffusion-webui/models/deepbooru"
-set dreambooth_dir="/opt/stable-diffusion-webui/models/dreambooth"
-set esrgan_models="/opt/stable-diffusion-webui/models/ESRGAN"
-set gfpgan_models="/opt/stable-diffusion-webui/models/GFPGAN"
-set hypernetworks="/opt/stable-diffusion-webui/models/hypernetworks"
-set insightface_models="/opt/stable-diffusion-webui/models/insightface"
-set karlo="/opt/stable-diffusion-webui/models/karlo"
-storage_map["stable_diffusion/models/ldsr"]="/opt/stable-diffusion-webui/models/LDSR"
-storage_map["stable_diffusion/models/lora"]="/opt/stable-diffusion-webui/models/Lora"
-storage_map["stable_diffusion/models/reactor"]="/opt/stable-diffusion-webui/models/reactor"
-storage_map["stable_diffusion/models/swinIR"]="/opt/stable-diffusion-webui/models/swinIR"
-storage_map["stable_diffusion/models/vae"]="/opt/stable-diffusion-webui/models/VAE"
-storage_map["stable_diffusion/models/vae_approx"]="/opt/stable-diffusion-webui/models/VAE-approx"
+set annotators_models_dir="/workspace/stable-diffusion-webui/models/annotators"
+set safetensors_models_dir="/workspace/stable-diffusion-webui/models/Stable-diffusion"
+set codeformer_models_dir="/workspace/stable-diffusion-webui/models/Codeformer"
+set controlnet_models_dir="/workspace/stable-diffusion-webui/models/ControlNet"
+set deepbooru_models_dir="/workspace/stable-diffusion-webui/models/deepbooru"
+set dreambooth_models_dir="/workspace/stable-diffusion-webui/models/dreambooth"
+set esrgan_models_dir="/workspace/stable-diffusion-webui/models/ESRGAN"
+set gfpga_models_dir="/opt/stable-diffusion-webui/models/GFPGAN"
+set hypernetworks_models_dir="/opt/stable-diffusion-webui/models/hypernetworks"
+set insightface_models_dir="/opt/stable-diffusion-webui/models/insightface"
+set karlo_models_dir="/opt/stable-diffusion-webui/models/karlo"
+set ldsr_models_dir="/opt/stable-diffusion-webui/models/LDSR"
+set lora_models_dir="/opt/stable-diffusion-webui/models/Lora"
+set reactor_models_dir="/opt/stable-diffusion-webui/models/reactor"
+set swinIR_modles_dir="/opt/stable-diffusion-webui/models/swinIR"
+set vae_models_dir="/opt/stable-diffusion-webui/models/VAE"
+set vae_approx_models_dir="/opt/stable-diffusion-webui/models/VAE-approx"
 
-cd ${ESRGAN}
+cd workspace/stable-diffusion-webui/models/
+mkdir annotators
+cd annotators
+git clone https://huggingface.co/lllyasviel/Annotators
+
+cd ${esrgan_models_dir}
 curl -o 4xUltraSharp.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/vRYVhaDA
 curl -o 4xUltraMix_Balanced.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
 curl -o 4xUltraMix_Restore.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
@@ -59,7 +65,7 @@ curl -o 16xESRGAN.pth https://objectstorage.us-phoenix-1.oraclecloud.com/n/ax6yg
 wget https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x-UltraSharp.pth
 Copied!
 
-cd ${lora}
+cd ${Lora_models_dir}
 https://civitai.com/models/292351
 https://civitai.com/models/234351
 https://civitai.com/models/274425
@@ -67,7 +73,8 @@ https://civitai.com/models/187082
 https://civitai.com/models/263993
 https://civitai.com/models/335970
 
-cd ${sontrolnet_models}
+cd ${controlnet_models_dir}
 git lfs install
+git clone https://huggingface.co/lllyasviel/sd_control_collection/tree/main
 git clone https://huggingface.co/lllyasviel/fav_models
-
+git clone https://huggingface.co/lllyasviel/control_v11e_sd15_ip2p
