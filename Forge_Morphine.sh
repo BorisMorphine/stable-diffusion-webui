e4db1df746d3367605ac814bb5ee4a40fb34734b
BorisMorphine <123861127+BorisMorphine@users.noreply.github.com>
2024-03-31 17:31:30 -0500

    Update Forge_Morphine.sh

--- Forge_Morphine.sh
+++ Forge_Morphine.sh
@@ -1,31 +1,130 @@
 #!/bin/bash
 
-# Install Instructions from Dev
-git remote add forge https://github.com/lllyasviel/stable-diffusion-webui-forge
+FROM nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04 as base
+
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+ENV DEBIAN_FRONTEND=noninteractive \
+    PYTHONUNBUFFERED=1 \
+    PIP_NO_CACHE_DIR=on \
+    SHELL=/bin/bash
+
+# Install Ubuntu packages
+RUN apt update && \
+    apt -y upgrade && \
+    apt install -y --no-install-recommends \
+        software-properties-common \
+        build-essential \
+        python3.10-venv \
+        python3-pip \
+        python3-tk \
+        python3-dev \
+        nginx \
+        bash \
+        dos2unix \
+        git \
+        ncdu \
+        net-tools \
+        openssh-server \
+        libglib2.0-0 \
+        libsm6 \
+        libgl1 \
+        libxrender1 \
+        libxext6 \
+        ffmpeg \
+        wget \
+        curl \
+        psmisc \
+        rsync \
+        vim \
+        zip \
+        unzip \
+        htop \
+        screen \
+        tmux \
+        bc \
+        pkg-config \
+        libcairo2-dev \
+        libgoogle-perftools4 \
+        libtcmalloc-minimal4 \
+        apt-transport-https \
+        ca-certificates && \
+    update-ca-certificates && \
+    apt clean && \
+    rm -rf /var/lib/apt/lists/* && \
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+
+# Set Python
+RUN ln -s /usr/bin/python3.10 /usr/bin/python
+
+# Stage 2: Install Stable Diffusion WebUI Forge and python modules
+FROM base as setup
+
+WORKDIR /
+RUN mkdir -p /sd-models
+
+# Add SDXL models and VAE
+# These need to already have been downloaded:
+#   wget https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/realisticVisionV51_v51VAE.safetensors
+COPY realisticVisionV51_v51VAE.safetensors /sd-models/realisticVisionV51_v51VAE.safetensors
+
+# Create and use the Python venv
+RUN python3 -m venv /venv
+
+# Clone the git repo of Stable Diffusion WebUI Forge and set version
+ARG FORGE_COMMIT
+RUN git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git && \
+    cd /stable-diffusion-webui-forge
+
+# Install the dependencies for Stable Diffusion WebUI Forge
+ARG INDEX_URL
+ARG TORCH_VERSION
+ARG XFORMERS_VERSION
+WORKDIR /stable-diffusion-webui-forge
+ENV TORCH_INDEX_URL=${INDEX_URL}
+ENV TORCH_COMMAND="pip install torch==${TORCH_VERSION} torchvision --index-url ${TORCH_INDEX_URL}"
+ENV XFORMERS_PACKAGE="xformers==${XFORMERS_VERSION} --index-url ${TORCH_INDEX_URL}"
+RUN source /venv/bin/activate && \
+    ${TORCH_COMMAND} && \
+    pip3 install -r requirements_versions.txt --extra-index-url ${TORCH_INDEX_URL} && \
+    pip3 install ${XFORMERS_PACKAGE} &&  \
+    deactivate
+
+COPY forge/cache-sd-model.py forge/install-forge.py ./
+RUN source /venv/bin/activate && \
+    python3 -m install-forge --skip-torch-cuda-test && \
+    deactivate
+
+# Cache the Stable Diffusion Models
+# SDXL models result in OOM kills with 8GB system memory, need 30GB+ to cache these
+    #RUN source /venv/bin/activate && \
+    #    python3 cache-sd-model.py --no-half-vae --no-half --xformers --use-cpu=all --ckpt /sd-models/realisticVisionV51_v51VAE.safetensors && \
+    #    deactivate
+
+# Copy Stable Diffusion WebUI Forge config files
+COPY forge/relauncher.py forge/webui-user.sh forge/config.json forge/ui-config.json /stable-diffusion-webui-forge/
+
+# ADD SDXL styles.csv
+ADD https://raw.githubusercontent.com/Douleb/SDXL-750-Styles-GPT4-/main/styles.csv /stable-diffusion-webui/styles.csv
+
+# install instrructions from dev SD FORGE
+cd /workspace/home/(whoami?)
+sudo git remote add forge https://github.com/lllyasviel/stable-diffusion-webui-forge
 git branch lllyasviel/main
 git checkout lllyasviel/main
 git fetch forge
 git branch -u forge/main
 git pull
 
-cd /extensions
-git remote add animateddiff https://github.com/continue-revolution/sd-forge-animatediff
-git branch forge-master
-git checkout forge-master
-git fetch forge
-git branch -u forge-master
-git pull
+models_dir="/opt/stable-diffusion-webui/models"
+models_dir="/opt/stable-diffusion-webui/extensions"
+safetensors_dir="/workspace/stable-diffusion-webui/models/Stable-diffusion"
+codeformer_dir="/workspace/stable-diffusion-webui/models/Codeformer"
+controlnet_dir="/workspace/stable-diffusion-webui/models/ControlNet"
+lora_dir="/opt/stable-diffusion-webui/models/Lora"
+ESRGAN_dir="/opt/stable-diffusion-webui/models/ESRGAN"
+_dirGFPGAN="/opt/stable-diffusion-webui/models/GFPGAN"
 
-# Setting download directory paths: 
-set models="/workspace/stable-diffusion-webui/models"
-set safetensors="/workspace/stable-diffusion-webui/models/Stable-diffusion"
-set codeformer_dir="/workspace/stable-diffusion-webui/models/Codeformer"
-set controlnet="/workspace/stable-diffusion-webui/models/ControlNet"
-set lora="/opt/stable-diffusion-webui/models/Lora"
-set ESRGAN="/opt/stable-diffusion-webui/models/ESRGAN"
-set GFPGAN="/opt/stable-diffusion-webui/models/GFPGAN"
-
-cd ${ESRGAN}
+cd ${ESRGAN_dir}
 wget -O 4xUltraSharp.pth https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x-UltraSharp.pth
 wget -O 4xUltraMix_Balanced.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
 wget -O 4xUltraMix_Restore.pth https://mega.nz/folder/qZRBmaIY#nIG8KyWFcGNTuMX_XNbJ_g/file/KBJRBQyR
@@ -40,7 +139,7 @@ wget -O 4xNomos8kDAT.pth https://drive.usercontent.google.com/download?id=1JRwXY
 wget -O 4xLSDIRplus.pth https://github.com/Phhofm/models/blob/main/4xLSDIRplus/4xLSDIRplus.pth
 wget -0 BSRGAN https://github.com/cszn/KAIR/releases/download/v1.0/BSRGAN.pth
 
-cd ${ckpt}
+cd ${ckpt_dir}
 wget DreamShaper_v2.1_TURBO_DPM++_SDE https://civitai.com/api/download/models/351306?type=Model&format=SafeTensor&size=full&fp=fp16
 
 cd ${lora}
