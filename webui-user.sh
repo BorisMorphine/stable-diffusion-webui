369f3924ab1d334a8954c79c22b33474d22958b2
BorisMorphine <123861127+BorisMorphine@users.noreply.github.com>
2024-04-01 10:45:46 -0500

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
-# Install directory without trailing slash
-install_dir="/opt"
-
-data_dir="/workspace"
+#########################################################
+# Uncomment and change the variables below to your need:#
+#########################################################
 
 # Name of the subdirectory
 clone_dir="stable-diffusion-webui"
 
+# Install directory without trailing slash
+install_dir="/workspace"
+
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--port 3001 --listen --api --xformers --enable-insecure-extension-access --no-half-vae"
+export COMMANDLINE_ARGS="--port 7860 --listen --api --xformers --enable-insecure-extension-access --no-half-vae"
 
 # python3 executable
 #python_cmd="python3"
@@ -18,7 +19,7 @@ export COMMANDLINE_ARGS="--port 3001 --listen --api --xformers --enable-insecure
 export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-venv_dir="/workspace/venv"
+venv_dir="/opt/micromamba/envs/webui/"
 
 # script to launch to start the app
 export LAUNCH_SCRIPT="launch.py"
@@ -27,11 +28,11 @@ export LAUNCH_SCRIPT="launch.py"
 export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
 # Requirements file to use for stable-diffusion-webui
-export REQS_FILE="requirements_versions.txt"
+export REQS_FILE="requirements.txt"
 
 # Fixed git repos
-#export K_DIFFUSION_PACKAGE=""
-#export GFPGAN_PACKAGE=""
+export K_DIFFUSION_PACKAGE="/Home/User/Sync"
+export GFPGAN_PACKAGE="/Home/User/Sync"
 
 # Fixed git commits
 #export STABLE_DIFFUSION_COMMIT_HASH=""
@@ -39,7 +40,7 @@ export REQS_FILE="requirements_versions.txt"
 #export BLIP_COMMIT_HASH=""
 
 # Uncomment to enable accelerated launch
-#export ACCELERATE="True"
+export ACCELERATE="True"
 
 # Uncomment to disable TCMalloc
 #export NO_TCMALLOC="True"
