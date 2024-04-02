9b7fae0a15eba45ce9ba8b3b46c6f29616baf628
BorisMorphine <123861127+BorisMorphine@users.noreply.github.com>
2024-04-02 01:34:50 -0500

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -3,18 +3,17 @@
 # Uncomment and change the variables below to your need:#
 #########################################################
 
+# Install directory without trailing slash
+install_dir="/workspace"
+
 # Name of the subdirectory
 clone_dir="stable-diffusion-webui"
 
-# Install directory without trailing slash
-#install_dir="/workspace"
-install_dir="opt"
+# Uncomment to enable accelerated launch
+export ACCELERATE="True"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
 export COMMANDLINE_ARGS="--port 7860 --listen --api --xformers --enable-insecure-extension-access --no-half-vae"
-
-# python3 executable
-#python_cmd="python3"
 
 # git executable
 export GIT="git"
@@ -22,14 +21,14 @@ export GIT="git"
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
 venv_dir="/opt/micromamba/envs/webui/"
 
-# script to launch to start the app
-export LAUNCH_SCRIPT="launch.py"
-
 # install command for torch
 export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
 # Requirements file to use for stable-diffusion-webui
 export REQS_FILE="requirements.txt"
+
+# script to launch to start the app
+export LAUNCH_SCRIPT="launch.py"
 
 # Fixed git repos
 #export K_DIFFUSION_PACKAGE="/Home/User/Sync"
@@ -39,9 +38,6 @@ export REQS_FILE="requirements.txt"
 #export STABLE_DIFFUSION_COMMIT_HASH=""
 #export CODEFORMER_COMMIT_HASH=""
 #export BLIP_COMMIT_HASH=""
-
-# Uncomment to enable accelerated launch
-#export ACCELERATE="True"
 
 # Uncomment to disable TCMalloc
 #export NO_TCMALLOC="True"
