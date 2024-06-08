a777b3f3dd16eb872f7dd3e09f221438ddb26022
BorisMorphine <123861127+BorisMorphine@users.noreply.github.com>
2024-06-08 12:01:48 -0500

    Update send_it.sh

--- send_it.sh
+++ send_it.sh
@@ -24,20 +24,7 @@ PIP_PACKAGES=(
 )
 
 EXTENSIONS=(
-    #"https://github.com/lllyasviel/ControlNet-v1-1-nightly"
-    #"https://github.com/Mikubill/sd-webui-controlnet"
-    #"https://github.com/d8ahazard/sd_dreambooth_extension"
-    #"https://github.com/deforum-art/sd-webui-deforum"
-    #"https://github.com/adieyal/sd-dynamic-prompts"
-    #"https://github.com/ototadana/sd-face-editor"
-    #"https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"
-    #"https://github.com/hako-mikan/sd-webui-regional-prompter"
-    #"https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
-    #"https://github.com/fkunn1326/openpose-editor"
-    #"https://github.com/Gourieff/sd-webui-reactor"
-    #"https://github.com/ibrahimsn98/sdwebui-kotlin"
-    #"https://github.com/lllyasviel/stable-diffusion-webui-forge"
-    #"https://github.com/s9roll7/sd_loopback_music_sync_wave"
+    "https://github.com/BlafKing/sd-civitai-browser-plus"
     "https://github.com/foglerek/SD-CN-Animation"
 )
 
@@ -63,6 +50,10 @@ ESRGAN_MODELS=(
 )
 
 CONTROLNET_MODELS=(
+    "https://civitai.com/api/download/models/20330?type=Model&format=SafeTensor&size=full&fp=fp16"
+    "https://civitai.com/api/download/models/20330?type=Config&format=Other"
+    "https://civitai.com/api/download/models/20331?type=Model&format=SafeTensor&size=full&fp=fp16"
+    "https://civitai.com/api/download/models/20331?type=Config&format=Other"
     "https://civitai.com/api/download/models/181335?type=Model&format=SafeTensor&size=full&fp=fp16"
     "https://civitai.com/api/download/models/507015?type=Model&format=SafeTensor"
     "https://civitai.com/api/download/models/183051?type=Model&format=SafeTensor"
